68141f3169d75ba78846e19c919fa714042366c4
Jack Hastings <32724063+JDHastings@users.noreply.github.com>
2023-06-02 14:25:56 -0400

    Operator support
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~
    
    May be a little buggy but it has the features I want.
    
    -~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~

--- javascript.js
+++ javascript.js
@@ -40,12 +40,14 @@ function percent(a){
 
 let num1 = 0;
 let num2 = '';
+let num3 = 0;
 let operator;
-let displayValue = 0;
+let displayValue = '';
 let previousValue;
 let isDecimal = false;
 let decimalCount = 0;
 let isNegative = false;
+let firstDecimal = true;
 
 function operate(a, b, op){
     if(op == '+'){
@@ -64,12 +66,15 @@ function updateDisplay(){
         num1 = Math.floor(num1 / 10);
     }else if(this.textContent == 'C'){
         num1 = 0;
+        num3 = 0;
         decimalCount = 0;
         isDecimal = false;
         isNegative = false;
     }else if(this.textContent == 'CE'){
         num1 = 0;
         num2 = '';
+        num3 = 0;
+        operator = '';
         isDecimal = false;
         decimalCount = 0;
         isNegative = false;
@@ -104,51 +109,52 @@ function updateDisplay(){
     }else{
         num1 = num1 * 10 + parseInt(this.textContent);
     }
-    calculatorDisplay.textContent = num1;
+    displayUpdate();
+}
+
+function operatorUpdate(){
+    if(!operator){
+        operator = this.textContent;
+        num3 = num1;
+        num1 = 0;
+    }else if(this.textContent == '='){
+        num2 = operate(num3, num1, operator);
+        num3 = 0;
+        num1 = 0;
+        operator = '';
+    }else{
+        num3 = operate(num3, num1, operator);
+        num1 = 0;
+        operator = this.textContent;
+    }
+    firstDecimal = true;
+    isDecimal = false;
+    decimalCount = 0;
+    displayUpdate();
+}
+
+function displayUpdate(){
+    if(operator){
+        displayValue = num3 + ' ' + operator + ' ' + num1;
+    }else if(isDecimal && firstDecimal){
+        firstDecimal = false;
+        displayValue = num1 + '.';  
+    }else{
+        displayValue = num1;
+    }
+    calculatorDisplay.textContent = displayValue;
     previousDisplay.textContent = num2;
 }
 
 const calculatorDisplay = document.querySelector('.calculator-display');
 const previousDisplay = document.querySelector('.previous-display');
 
-const displayZero = document.querySelector('.zero');
-const displayOne = document.querySelector('.one');
-const displayTwo = document.querySelector('.two');
-const displayThree = document.querySelector('.three');
-const displayFour = document.querySelector('.four');
-const displayFive = document.querySelector('.five');
-const displaySix = document.querySelector('.six');
-const displaySeven = document.querySelector('.seven');
-const displayEight = document.querySelector('.eight');
-const displayNine = document.querySelector('.nine');
-const backspace = document.querySelector('.delete');
-const clear = document.querySelector('.clear');
-const clearAll = document.querySelector('.clear-everything');
-const reciporicalVar = document.querySelector('.reciporical');
-const squaredVar = document.querySelector('.squared');
-const sqrtVar = document.querySelector('.sqrt');
-const percentVar = document.querySelector('.percent');
-const decimal = document.querySelector('.decimal');
-const negative = document.querySelector('.negative');
+const displayNumber = document.querySelectorAll('.display-number');
 
-displayZero.addEventListener('click', updateDisplay);
-displayOne.addEventListener('click', updateDisplay);
-displayTwo.addEventListener('click', updateDisplay);
-displayThree.addEventListener('click', updateDisplay);
-displayFour.addEventListener('click', updateDisplay);
-displayFive.addEventListener('click', updateDisplay);
-displaySix.addEventListener('click', updateDisplay);
-displaySeven.addEventListener('click', updateDisplay);
-displayEight.addEventListener('click', updateDisplay);
-displayNine.addEventListener('click', updateDisplay);
-backspace.addEventListener('click', updateDisplay);
-clear.addEventListener('click', updateDisplay);
-clearAll.addEventListener('click', updateDisplay);
-reciporicalVar.addEventListener('click', updateDisplay);
-squaredVar.addEventListener('click', updateDisplay);
-sqrtVar.addEventListener('click', updateDisplay);
-percentVar.addEventListener('click', updateDisplay);
-decimal.addEventListener('click', updateDisplay);
-negative.addEventListener('click', updateDisplay);
+displayNumber.forEach(number => number.addEventListener('click', updateDisplay));
+
+const displayOperator = document.querySelectorAll('.operator');
+
+displayOperator.forEach(number => number.addEventListener('click', operatorUpdate));
 
 // next add all the operators: divide multiply subtract add and equals
